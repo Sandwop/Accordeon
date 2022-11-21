5a5c25f0b0a09e227186d9427e0dc762c8bc654b
Sandwop <74904204+Sandwop@users.noreply.github.com>
2022-11-21 18:42:43 +0100

    Maj

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,11 +1,11 @@
-
+import woman from './img/woman.svg'
 
 function App() {
   return (
     <div className="app">
       <div className="container">
         <div className="column colLeft">
-
+          <img className='imgWoman' src={woman} />
         </div>
         <div className="column colRight">
 
